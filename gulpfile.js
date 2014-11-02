66cbdb8c5d836a005dd3740dcf423b38fde6dce3
Christian Kohler <kohler.christian@gmail.com>
2014-11-02 14:38:28 +0100

    gulp tasks for test&tdd added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var concat = require('gulp-concat');
+var karma = require('karma').server;
 
 var paths = {
   sourcefiles : [
@@ -22,6 +23,29 @@ gulp.task('release', function() {
     .pipe(gulp.dest('./release/'))
     .pipe(gulp.dest('./examples/'))
 });
+
+/**
+* Runs our unit tests with karma
+* */
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+    autoWatch: false
+  }, done);
+});
+
+/**
+ * Watch for file changes and re-run tests on each change
+ * */
+gulp.task('tdd', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    autoWatch: true
+  }, done);
+});
+
+
 
 gulp.task('watch', function(){
   gulp.watch(paths.sourcefiles, ['release']);
