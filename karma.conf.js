3b1f90e220e5d891ba1aa7dbde666dd04340a394
Christian Kohler <kohler.christian@gmail.com>
2014-11-02 18:16:28 +0100

    Firefox launcher added

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // PhantomJS is not possible at the moment due the lack of webcomponents support
-    browsers: ['Chrome'],
+    browsers: ['Firefox',],
 
 
     // Continuous Integration mode
