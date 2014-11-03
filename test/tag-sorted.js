50e8bf3cc697c212e1eb97f424976ad4630b9877
Stefan Barthel <bartix@hotmail.de>
2014-11-04 00:57:30 +0100

    failing test

--- test/tag-sorted.js
+++ test/tag-sorted.js
@@ -3,6 +3,6 @@
 describe('tag-sorted', function () {
     it('should be tested with karma', function () {
         console.log('I was in tag-sorted test!');
-        expect(true).toBe(true);
+        expect(true).toBe(false);
     });
 });
