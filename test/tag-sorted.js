90c26ae46613d11c5ee8e42e788cbbf6049157cf
Stefan Barthel <bartix@hotmail.de>
2014-11-04 00:58:49 +0100

    one positive test also

--- test/tag-sorted.js
+++ test/tag-sorted.js
@@ -3,6 +3,11 @@
 describe('tag-sorted', function () {
     it('should be tested with karma', function () {
         console.log('I was in tag-sorted test!');
+        expect(true).toBe(true);
+    });
+
+    it('fails for checking out travis', function () {
+        console.log('I was in failing test!');
         expect(true).toBe(false);
     });
 });
